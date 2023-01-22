27b6a142faf97dfed5b0fd195466d6cef236c871
Chris Balla <ballachris13@gmail.com>
2023-01-23 00:09:03 +0100

    make a route an link to it store blog posts as html files route wildcard and add constraints

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Consequat dolor consectetur aute mollit incididunt deserunt officia veniam non. Irure nostrud Lorem esse reprehenderit. Sint duis nulla tempor velit Lorem Lorem ex laborum reprehenderit consequat occaecat qui. Est anim qui ullamco ipsum. Do tempor labore nisi ex velit id nostrud est dolor elit laboris mollit. Non duis in voluptate est culpa mollit ullamco culpa eu enim cillum.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>Consequat dolor consectetur aute mollit incididunt deserunt officia veniam non. Irure nostrud Lorem esse reprehenderit. Sint duis nulla tempor velit Lorem Lorem ex laborum reprehenderit consequat occaecat qui. Est anim qui ullamco ipsum. Do tempor labore nisi ex velit id nostrud est dolor elit laboris mollit. Non duis in voluptate est culpa mollit ullamco culpa eu enim cillum.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p>Consequat dolor consectetur aute mollit incididunt deserunt officia veniam non. Irure nostrud Lorem esse reprehenderit. Sint duis nulla tempor velit Lorem Lorem ex laborum reprehenderit consequat occaecat qui. Est anim qui ullamco ipsum. Do tempor labore nisi ex velit id nostrud est dolor elit laboris mollit. Non duis in voluptate est culpa mollit ullamco culpa eu enim cillum.</p>
+    </article>
+
+</body>
+
+</html>
